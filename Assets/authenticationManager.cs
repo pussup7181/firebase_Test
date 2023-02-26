83f0f8555076b3b537e693b2bb7d66f7eea733d7
Pushpendra Sharma <pushpendrash718@gmail.com>
2023-02-26 15:18:46 +0530

    Email Login and verification part 1

--- Assets/authenticationManager.cs
+++ Assets/authenticationManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,50 +7,25 @@ using Firebase.Auth;
 
 public class authenticationManager : MonoBehaviour
 {
-    public TMP_InputField Email;
-    public TMP_InputField password;
+    //Firebase
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+
+    [Space]
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
     public Button signup;
-    FirebaseAuth auth;
-    // Start is called before the first frame update
-    void Start()
-    {
-         auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-        Email.onValueChanged.AddListener(delegate
-        {
-            Debug.Log(Email.text);
-            if (Email.text.Contains("@"))
-            {
-                
-            }
-        });
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Space]
+    [Header("Registration")]
+    public TMP_InputField nameRegistrationField;
+    public TMP_InputField emailRegistrationField;
+    public TMP_InputField passwordRegistrationField;
+    public TMP_InputField confirmPasswordRegistrationField;
 
-   public void signUp()
-    {
-        auth.CreateUserWithEmailAndPasswordAsync(Email.text, password.text).ContinueWith(task => {
-            if (task.IsCanceled)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
-                return;
-            }
-            if (task.IsFaulted)
-            {
-                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
-                return;
-            }
 
-            // Firebase user has been created.
-            FirebaseUser newUser = task.Result;
-            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
-                newUser.DisplayName, newUser.UserId);
-            
-        });
-    }
+   
 }
